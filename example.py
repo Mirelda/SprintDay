4104130676087c62ea7671ff0b82643b035da577
Mirelda <mireldadiker@gmail.com>
2021-05-06 12:09:41 +0300

    Make_Response

--- example.py
+++ example.py
@@ -4,6 +4,7 @@ from flask import Flask, jsonify, request, make_response
 
 app = Flask(__name__)
 
+
 @app.route('/', methods = ['GET'])
 def hi():
     return "<h1>Hello World!<h1>"
@@ -12,14 +13,16 @@ def hi():
 @app.route('/name', methods = ['GET'])
 def name():
     x = request.args
-    return jsonify({'firstname': x.get('firstname',''),
+
+    if(request.method == 'Post' or request.method == 'PUT'):
+
+        return make_response(jsonify(Success= False, Error='405 Method Not Allowed.'),405)
+        
+    
+    else:
+    
+        return jsonify({'firstname': x.get('firstname',''),
             'lastname':x.get('lastname','')})
-
-
-@app.route("/check")
-def check():
-    return make_response(jsonify(Success=True), 200)
-
-
+    
 if __name__ == "__main__":
     app.run(host = '0.0.0.0', port ='8080')
